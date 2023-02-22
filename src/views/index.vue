27a1a447df3a5bfcd72a93ca5f19b4be35fb6877
ohh <15296670997@163.com>
2023-02-22 13:51:31 +0800

    feat：add router 、element-ui

---
+++ src/views/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>{{ msg }}</div>
+</template>
+
+<script>
+export default {
+  name: 'index',
+  data() {
+    return {
+      msg: "hello World",
+    };
+  },
+};
+</script>
+
+<style lang="less" scoped></style>
